0b3514479ec3d14bee27e859a6608e8592c7d488
tjlan <tjlan@TANNERLAPTOP>
2024-01-23 09:31:19 -0500

    Add project files.

---
+++ FinalProject/main.cpp
@@ -0,0 +1,25 @@
+#include<SFML/Graphics.hpp>
+
+
+int main()
+{
+	/*Creating Window*/
+	sf::RenderWindow window(sf::VideoMode(1920, 1080), "Final Project");
+
+
+	/*Gameloop*/
+	while (window.isOpen())
+	{
+		/*Event*/
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			/*Exit*/
+			if (event.type == sf::Event::Closed)
+			{
+				window.close();
+			}
+		}
+	}
+
+}
