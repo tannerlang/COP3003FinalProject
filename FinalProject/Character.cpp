7ad8df1f9026498215f62600580e3a4932cb282e
Tanner Lang <tjlang1120@gmail.com>
2024-01-26 21:24:09 -0500

    added attributes and core stats to character.h and character.cpp class

--- FinalProject/Character.cpp
+++ FinalProject/Character.cpp
@@ -3,6 +3,24 @@ Character::Character()
 {
 	set_Health(100);
 
+	//init core stats
+	int max_health = 100;
+	int max_stamina = 100;
+	int stamina = 50;
+	int damage = 50;
+	bool dodge;
+	int speed = 50;
+	int attack_speed = 50;
+	float crit_chance = 0.1;
+	int level = 1;
+	int exp = 0;
+
+	//init base attributes
+	int vitality = 20;	
+	int strength = 20;	
+	int agility = 20;	
+	int dexterity = 20;
+
 }
 
 Character::~Character()
--- FinalProject/Character.h
+++ FinalProject/Character.h
@@ -2,13 +2,36 @@
 class Character
 {
 private:
+	//core stats
 	int health;
+	int max_health;
+	int level;
+	int exp;
+
+	int max_stamina;
+	int stamina;
+	int damage;
+	bool dodge;
+	int speed;
+	int attack_speed;
+	float crit_chance;
+
+	//attributes
+	int vitality;	//health and stamina
+	int strength;	//damage
+	int agility;	//dodge and speed
+	int dexterity;	//attack-speed and crit
+	
+
+
+
+
+	
 public:
 	Character();
 	~Character();
 	int get_Health();
 	void set_Health(int h);
-
 
 
 };
