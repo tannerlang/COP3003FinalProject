e7f2e5cde65ec2b23349bc81103d39783bde74f0
Tanner Lang <tjlang1120@gmail.com>
2024-03-26 09:08:50 -0400

    small change

--- FinalProject/Character.cpp
+++ FinalProject/Character.cpp
@@ -4,22 +4,22 @@ Character::Character()
 	set_Health(100);
 
 	//init core stats
-	int max_health = 100;
-	int max_stamina = 100;
-	int stamina = 50;
-	int damage = 50;
-	bool dodge;
-	int speed = 50;
-	int attack_speed = 50;
-	float crit_chance = 0.1;
-	int level = 1;
-	int exp = 0;
+	max_health = 100;
+	max_stamina = 100;
+	stamina = 50;
+	damage = 50;
+	dodge;
+	speed = 50;
+	attack_speed = 50;
+	crit_chance = 0.1;
+	level = 1;
+	exp = 0;
 
 	//init base attributes
-	int vitality = 20;	
-	int strength = 20;	
-	int agility = 20;	
-	int dexterity = 20;
+	vitality = 20;	
+	strength = 20;	
+	agility = 20;	
+	dexterity = 20;
 
 }
 
