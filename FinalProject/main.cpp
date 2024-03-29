6888fb801e0b456d31adf6cbffcf6124246fe0ae
Tanner Lang <tjlang1120@gmail.com>
2024-03-29 18:16:23 -0400

    added a not

--- FinalProject/main.cpp
+++ FinalProject/main.cpp
@@ -9,4 +9,5 @@ int main()
 	PlayGame.run();
 
 		//TODO:figure out how to have class selection so that in the game class files we are not just calling brawler, we need to be able to call archer too and it be dynamic.
+		//do we have to create a hybid inheritance class that derives from Brawler and Archer? 
 }
