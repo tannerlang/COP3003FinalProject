fd48446402fe4f1ea247daef7c55c82f36f40f15
Tanner Lang <tjlang1120@gmail.com>
2024-03-29 12:29:49 -0400

    working on encapsulating main gameloop into a class

--- FinalProject/game.cpp
+++ FinalProject/game.cpp
@@ -1,7 +1,17 @@
 #include "game.h"
 
+
+//private function
+void Game::initWindow()
+{
+	this->window = new sf::RenderWindow(sf::VideoMode(windowWidth, windowHeight), "Final Project");
+}
+
+
 Game::Game()
 {
+	this->initWindow();
+	gameWorld gameWorld1 = gameWorld();
 
 }
 
--- FinalProject/game.h
+++ FinalProject/game.h
@@ -7,7 +7,11 @@
 class Game
 {
 private: 
+
+	float windowWidth = 1920;
+	float windowHeight = 1080;
 	sf::RenderWindow* window;
+	void initWindow();
 
 public:
 	Game();
